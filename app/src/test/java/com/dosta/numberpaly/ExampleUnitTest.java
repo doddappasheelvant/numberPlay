cf38d9aa8e64ebad36d675aa207d96215d8b3f91
DoddappaSheelvant <61374077+doddappasheelvant@users.noreply.github.com>
2024-09-28 15:06:55 +0530

    Update ExampleUnitTest.java

--- app/src/test/java/com/dosta/numberpaly/ExampleUnitTest.java
+++ app/src/test/java/com/dosta/numberpaly/ExampleUnitTest.java
@@ -6,7 +6,6 @@ import static org.junit.Assert.*;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
- *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
@@ -14,4 +13,5 @@ public class ExampleUnitTest {
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
     }
+    
 }
